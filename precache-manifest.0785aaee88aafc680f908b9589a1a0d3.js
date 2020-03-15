26c99e7bd804e9eb99029cec59088d0282681660
GB <gb@empatica.com>
2020-03-15 15:51:40 +0100

    Updates

---
+++ precache-manifest.0785aaee88aafc680f908b9589a1a0d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a0542c9854946489c5792e26cdd0edb",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "96dc63e356e4cee604bd",
+    "url": "/covid19/static/css/main.25ac248d.chunk.css"
+  },
+  {
+    "revision": "a18a63bce0b114bff57d",
+    "url": "/covid19/static/js/2.788d541b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/covid19/static/js/2.788d541b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96dc63e356e4cee604bd",
+    "url": "/covid19/static/js/main.d1135322.chunk.js"
+  },
+  {
+    "revision": "643516e5905acfb09596",
+    "url": "/covid19/static/js/runtime-main.2ff68d4b.js"
+  }
+]);
