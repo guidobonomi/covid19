e68b5a154850277e301d61c15e01f5fd219cea41
GB <gb@empatica.com>
2020-03-15 15:41:31 +0100

    Updates

---
+++ precache-manifest.c61788ac9eba91d740a2e101986d8682.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9824f9e40df65540a867",
+    "url": "/static/js/main.9824f9e4.chunk.js"
+  },
+  {
+    "revision": "4699c41621f98d6948e4",
+    "url": "/static/js/1.4699c416.chunk.js"
+  },
+  {
+    "revision": "9824f9e40df65540a867",
+    "url": "/static/css/main.ef72a785.chunk.css"
+  },
+  {
+    "revision": "a08a96a9957b9a20edee2cb875cfe057",
+    "url": "/index.html"
+  }
+];
