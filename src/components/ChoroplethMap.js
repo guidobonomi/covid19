911f173029e37ef39e7c80a379d1dee74b480c2f
GB <gb@empatica.com>
2020-03-15 16:50:50 +0100

    Refactoring

--- src/components/ChoroplethMap.js
+++ src/components/ChoroplethMap.js
@@ -5,75 +5,96 @@ import topologyJson from './topology.json';
 
 class ChoroplethMap extends Component {
     componentDidMount() {
-        fetch('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province.json')
+        const yesterday = new Date(new Date().setDate(new Date().getDate() - 1))
+            .toISOString().substring(0, 10);
+
+        this.fetchData(yesterday)
+            .then(this.createDataSet)
+            .then(this.createDataMap);
+    }
+
+    fetchData = (date) => {
+        return fetch('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-province.json')
             .then((response) => {
                 return response.json();
             })
             .then((d) => {
-                const myMap = d.filter(d => {
-                    const yesterday = new Date(new Date().setDate(new Date().getDate() - 1))
-                        .toISOString().substring(0, 10);
-                    return d.data.startsWith(yesterday) && d.sigla_provincia;
+                const perProvinceMap = d.filter(d => {
+                    return d.sigla_provincia && d.totale_casi;
+                });
+
+                const allValues = perProvinceMap.map(d => {
+                    return d.totale_casi
+                });
+
+                const dataMap = perProvinceMap.filter(d => {
+                    return d.data.startsWith(date);
                 }).map(d => {
                     return [d.sigla_provincia, d.totale_casi]
                 })
 
-                const data = Array.from(myMap.values());
+                return {
+                    min: Math.min.apply(Math, allValues),
+                    max: Math.max.apply(Math, allValues),
+                    values: Array.from(dataMap.values()),
+                };
+            })
+    }
 
-                let uniqValues = data.map(function (obj) { return obj[1]; });
-                let minValue = Math.min.apply(null, uniqValues),
-                    maxValue = Math.max.apply(null, uniqValues);
+    createDataSet = (data) => {
+        let paletteScale = d3.scale.linear()
+            .domain([data.min, data.max])
+            .range(["#FFFFDD", "red"]);
 
-                let paletteScale = d3.scale.linear()
-                    .domain([minValue, maxValue])
-                    .range(["#FFFFDD", "red"]);
+        let dataset = {};
 
-                let dataset = {};
+        data.values.forEach(function (item) {
+            const iso = item[0],
+                value = item[1];
+            dataset[iso] = {
+                numberOfThings: value,
+                fillColor: paletteScale(value)
+            };
+        });
 
-                data.forEach(function (item) {
-                    const iso = item[0],
-                        value = item[1];
-                    dataset[iso] = {
-                        numberOfThings: value,
-                        fillColor: paletteScale(value)
-                    };
-                });
+        return dataset;
+    }
 
-                new Datamap({
-                    element: document.getElementById('cloropleth_map'),
-                    scope: 'italy',
-                    geographyConfig: {
-                        popupOnHover: true,
-                        highlightOnHover: true,
-                        borderColor: '#444',
-                        highlightBorderWidth: 1,
-                        borderWidth: 0.5,
-                        dataJson: topologyJson,
-                        popupTemplate: function (geo, data) {
-                            let numberOfThings = data ? data.numberOfThings : 0;
-                            // tooltip content
-                            return ['<div class="hoverinfo">',
-                                '<strong>', geo.properties.name, '</strong>',
-                                '<br>Count: <strong>', numberOfThings, '</strong>',
-                                '</div>'].join('');
-                        }
-                    },
-                    fills: {
-                        UNKNOWN: 'rgb(0,0,0)',
-                        defaultFill: 'white'
-                    },
-                    data: dataset,
-                    setProjection: function (element) {
-                        var projection = d3.geo.mercator()
-                            .center([13, 42])
-                            .scale(2500)
-                            .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+    createDataMap = (dataset) => {
+        new Datamap({
+            element: document.getElementById('cloropleth_map'),
+            scope: 'italy',
+            geographyConfig: {
+                popupOnHover: true,
+                highlightOnHover: true,
+                borderColor: '#444',
+                highlightBorderWidth: 1,
+                borderWidth: 0.5,
+                dataJson: topologyJson,
+                popupTemplate: function (geo, data) {
+                    let numberOfThings = data ? data.numberOfThings : 0;
+                    // tooltip content
+                    return ['<div class="hoverinfo">',
+                        '<strong>', geo.properties.name, '</strong>',
+                        '<br>Count: <strong>', numberOfThings, '</strong>',
+                        '</div>'].join('');
+                }
+            },
+            fills: {
+                UNKNOWN: 'rgb(0,0,0)',
+                defaultFill: 'white'
+            },
+            data: dataset,
+            setProjection: function (element) {
+                var projection = d3.geo.mercator()
+                    .center([13, 42])
+                    .scale(2500)
+                    .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
 
-                        var path = d3.geo.path().projection(projection);
-                        return { path: path, projection: projection };
-                    }
-                });
-            });
+                var path = d3.geo.path().projection(projection);
+                return { path: path, projection: projection };
+            }
+        });
     }
 
     render() {
